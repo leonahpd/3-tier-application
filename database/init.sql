cffb6e9aeadb21a69945d327668225e47fd7e36b
leonahpd <lionaraj97@gmail.com>
2025-02-08 18:31:12 +0530

    Update

--- database/init.sql
+++ database/init.sql
@@ -1,13 +1,4 @@
-CREATE DATABASE shopping_db;
-
-USE shopping_db;
-
-CREATE TABLE items (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    price VARCHAR(255)
-);
-
-INSERT INTO items (name, price) VALUES ('Laptop', '$1000');
-INSERT INTO items (name, price) VALUES ('Headphones', '$200');
-INSERT INTO items (name, price) VALUES ('Smartphone', '$800');
+CREATE DATABASE testdb;
+USE testdb;
+CREATE TABLE messages (id INT AUTO_INCREMENT PRIMARY KEY, message VARCHAR(255));
+INSERT INTO messages (message) VALUES ('Hello from MySQL Database!');
