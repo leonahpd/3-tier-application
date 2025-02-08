06eb7117b79d51420c4725fc4c35ba0a07347df8
leonahpd <lionaraj97@gmail.com>
2025-02-08 21:44:59 +0530

    Update init.sql

--- database/init.sql
+++ database/init.sql
@@ -1,5 +1,11 @@
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
 USE testdb;
-CREATE TABLE messages (id INT AUTO_INCREMENT PRIMARY KEY, message VARCHAR(255));
-INSERT INTO messages (message) VALUES ('Hello from MySQL Database!');
+
+CREATE TABLE messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    message VARCHAR(255) NOT NULL
+);
+
+INSERT INTO messages (id, message) VALUES (1, 'Hello from MySQL Database!')
+ON DUPLICATE KEY UPDATE message='Hello from MySQL Database!';
